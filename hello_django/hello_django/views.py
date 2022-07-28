01b23f6e43b932ad7c1cbd75048673ebdfb389ac
aleksej <albel7676@gmail.com>
2022-07-28 10:05:11 +0300

    start_files

---
+++ hello_django/hello_django/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def about(request):
+    return render(request, 'about.html')
+
+def reverse(request):
+    user_text = request.GET['username']
+    reverse = user_text[::-1]
+    return render(request, 'reverse.html', {'word': reverse})
